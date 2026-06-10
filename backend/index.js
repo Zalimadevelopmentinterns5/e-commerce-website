8420350b3c8ccfcfb1be7814d1b36961762d29f1
deva shermale <shermaledeva@gmail.com>
2026-06-10 11:06:39 +0530

    feat: set up Express server and connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+const mongoose = require('mongoose')
+const userroute = require('./route/userroute')
+app.use(express.json())
+app.use('/api/user', userroute)
+
+mongoose.connect('mongodb://localhost:27017/e-commerce')
+.then(() => {
+    console.log('connected to database')
+    app.listen(5000, () => {
+        console.log('http://localhost:5000')
+    }
+    )
+})
