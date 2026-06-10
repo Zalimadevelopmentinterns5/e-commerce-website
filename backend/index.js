36a91afc17a03f8a8d52d91746ee354855c5f17f
deva shermale <shermaledeva@gmail.com>
2026-06-10 11:49:51 +0530

    feat: add product and order management routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
-
 const app = express()
 const mongoose = require('mongoose')
-const userroute = require('./route/userroute')
 app.use(express.json())
+
+const userroute = require('./route/userroute')
+const productroute = require('./route/productroute')
+const orderroute = require('./route/orderroute')
+
+
 app.use('/api/user', userroute)
+app.use('/api/product', productroute)
+app.use('/api/order', orderroute)
+
+const PORT = process.env.PORT 
 
 mongoose.connect('mongodb://localhost:27017/e-commerce')
 .then(() => {
