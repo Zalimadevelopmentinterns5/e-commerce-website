3051c8d434c5a436c239a99bcb99bde829344373
deva shermale <shermaledeva@gmail.com>
2026-06-27 17:35:25 +0530

    Update MongoDB connection URI and set default port

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,10 @@ app.use('/api/user', userroute)
 app.use('/api/product', productroute)
 app.use('/api/order', orderroute)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
-mongoose.connect('mongodb://localhost:27017/e-commerce')
+
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log('connected to database')
     app.listen(PORT, () => {
